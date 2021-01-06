cb655de1f77222b601649784b027d58fe9c72e04
YoungMayor <youngmayor08@gmail.com>
2021-01-06 13:36:56 +0100

    UI modifications to the header nav

--- assets/js/general.js
+++ assets/js/general.js
@@ -6,4 +6,16 @@ $(function() {
     $('.close-mobile-navig').click(function() {
         $('body').removeClass('show-mobile-menu');
     });
+
+    $('.header-nav-dropdown > a').hover(function() {
+        // over
+        // alert('shown')
+        $(this).addClass('show');
+        $(this).sibling('.dropdown-menu').addClass('show');
+    }, function() {
+        // out
+        // alert('out')
+        $(this).removeClass('show');
+        $(this).sibling('.dropdown-menu').removeClass('show');
+    });
 })
